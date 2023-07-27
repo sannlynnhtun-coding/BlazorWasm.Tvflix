bd3dd26bf7ffb6811e605f5707beb02fe67c2d30
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-07-27 21:25:28 +0630

    Add project files.

---
+++ BlazorWasm.Tvflix/Program.cs
@@ -0,0 +1,11 @@
+using BlazorWasm.Tvflix;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient());
+
+await builder.Build().RunAsync();
